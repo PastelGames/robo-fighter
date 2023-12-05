83d2dcce0f3c58d50e8a8790a8ab745c8ce7d4bc
El'Christopher Tucker <elchristopher@outlook.com>
2023-12-05 01:45:38 -0600

    Jump direction is only set by player and not velocity.

--- Assets/Scripts/State Machine/ActionScripts/JumpAction.cs
+++ Assets/Scripts/State Machine/ActionScripts/JumpAction.cs
@@ -9,31 +9,35 @@ public class JumpAction : Action
     private float _horizontalVelocity;
     private Rigidbody2D _rb;
     private Fighter _fighter;
-    public int jumpFrames;
 
     public override void Act(StateController controller)
     {
         _rb.velocity = new Vector2(_horizontalVelocity, _rb.velocity.y);
         _fighter = controller as Fighter;
-        jumpFrames++;
         if (_fighter.isGrounded) controller.TransitionToState(controller.previousState);
     }
 
     public override void EndAct(StateController controller)
     {
-        Debug.Log(jumpFrames);
+        _fighter.SetFacing();
     }
 
     public override void StartAct(StateController controller)
     {
         _rb = controller.GetComponent<Rigidbody2D>();
         FighterController fighterController = controller.GetComponent<FighterController>();
-        _horizontalVelocity = _rb.velocity.x;
+        if (fighterController.GetMoveValueHorizontal() == 0)
+        {
+            _horizontalVelocity = 0;
+        }
+        else
+        {
+            _horizontalVelocity = _rb.velocity.x;
+        }
         _rb.velocity = new Vector2(_horizontalVelocity, _jumpInitialUpVelocity);
         AnimancerComponent ac = controller.GetComponent<AnimancerComponent>();
         ac.Play(_jumpAnimationClip).Time = 0;
         _fighter = controller as Fighter;
         _fighter.isGrounded = false;
-        jumpFrames = 0;
     }
 }
